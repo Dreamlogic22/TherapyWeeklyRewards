32732d75f53ed21ec3631730d0222ea584306441
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2026-02-11 16:23:01 +0500

    Create ruRU.lua

---
+++ Locales/ruRU.lua
@@ -0,0 +1,13 @@
+---@type string, TherapyWeeklyRewards
+local _, T = ...
+
+---@class Locale
+local L = {}
+
+L.CATALYST_CHARGES = "У Вас доступен %s Катализатор |4заряд:заряда:зарядов;"
+L.HIDE_MINIMAP_BUTTON = "Скрыть иконку у миникарты"
+L.LOCK_MINIMAP_BUTTON = "Заблокировать иконку на миникарте"
+L.REWARDS_AVAILABLE = "Доступны награды!"
+L.WEEKLY_REWARDS = "Еженедельные награды"
+
+T.Locale = L
